ef4b69bc13da291c199cecb6c1a86e97d70abba9
karthik kannan <karthikshaastradistro@gmail.com>
2012-04-02 15:18:55 +0400

    Abhik's working version

---
+++ ui/LibraryTableModel.java
@@ -0,0 +1,131 @@
+package iitm.apl.player.ui;
+
+import iitm.apl.player.Song;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Table model for a library
+ * 
+ */
+public class LibraryTableModel extends AbstractTableModel {
+	private static final long serialVersionUID = 8230354699902953693L;
+
+	// TODO: Change to your implementation of Trie/BK-Tree
+	private Vector<Song> songListing;
+	private int songIteratorIdx;
+	private Song currentSong;
+	private Iterator<Song> songIterator;
+
+	LibraryTableModel() {
+		songListing = new Vector<Song>();
+		songIterator = songListing.iterator();
+	}
+
+	public Vector<Song> getSong(){
+		return songListing;
+	}
+	public void add(Song song) {
+		songListing.add(song);
+		resetIdx();
+		fireTableDataChanged();
+	}
+	public void add(ArrayList<Song> array) {
+		for(int i=0; i<array.size();i++){
+		add(array.get(i));
+		}
+		resetIdx();
+		fireTableDataChanged();
+	}
+
+	public void add(Vector<Song> songs) {
+		songListing.addAll(songs);
+		resetIdx();
+		fireTableDataChanged();
+	}
+
+	public void filter(String searchString) {
+		// TODO: Connect the searchText keyPressed handler to update the filter
+		// here.
+	}
+	
+	public void resetIdx()
+	{
+		songIteratorIdx = -1;
+		currentSong = null;
+		songIterator = songListing.iterator();
+	}
+	// Gets the song at the currently visible index
+	public Song get(int idx) {
+		if( songIteratorIdx == idx )
+			return currentSong;
+		
+		if(songIteratorIdx > idx)
+		{
+			resetIdx();
+		}
+		while( songIteratorIdx < idx && songIterator.hasNext() )
+		{
+			currentSong = songIterator.next();
+			songIteratorIdx++;
+		}
+		return currentSong;
+	}
+	public void deleteAll(){
+			songListing.clear();
+	}
+	@Override
+	public int getColumnCount() {
+		// Title, Album, Artist, Duration.
+		return 4;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO: Changes if you've filtered the list
+		return songListing.size();
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		// TODO: Get the appropriate row
+		Song song = get(row);
+		if(song == null) return null;
+
+		switch (col) {
+		case 0: // Title
+			return song.getTitle();
+		case 1: // Album
+			return song.getAlbum();
+		case 2: // Artist
+			return song.getArtist();
+		case 3: // Duration
+			int duration = song.getDuration();
+			int mins = duration / 60;
+			int secs = duration % 60;
+			return String.format("%d:%2d", mins, secs);
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0: // Title
+			return "Title";
+		case 1: // Album
+			return "Album";
+		case 2: // Artist
+			return "Artist";
+		case 3: // Duration
+			return "Duration";
+		default:
+			return super.getColumnName(column);
+		}
+	}
+
+}
---
+++ ui/TrieNode.java
@@ -0,0 +1,172 @@
+package iitm.apl.player.ui;
+
+import iitm.apl.player.Song;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Vector;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+@SuppressWarnings("unchecked")
+public class TrieNode implements Comparable {
+	// to denote whether any word /string ends at this node
+	private boolean end = false;
+	// array of array lists storing the nodes(chain hashing)
+	private ArrayList<TrieNode> children;
+	// to denote the letter
+	private char letter = 0;
+	private Song song;
+
+	public TrieNode() {
+		children = new ArrayList();
+	}
+
+	public void add(Vector<Song> a) {
+		for (int i = 0; i < a.size(); i++) {
+			if (a.get(i) != null)
+				this.add(a.get(i).getTitle().toLowerCase(), a.get(i));
+		}
+	}
+
+	// constructor with the specific letter
+	public TrieNode(char x) {
+		this.letter = x;
+	}
+
+	public void add(String word, Song song) {
+		if (word.length() == 0)
+			return;
+		char current = word.charAt(0);
+		if (bin_search(current, this.children, 0, this.children.size() - 1) == null) {
+			TrieNode trie_node = new TrieNode();
+			trie_node.letter = current;
+			children.add(trie_node);
+			Collections.sort(children);
+		}
+		TrieNode temp = bin_search(current, this.children, 0, this.children
+				.size() - 1);
+		if (word.length() > 1) {
+			temp.add(word.substring(1), song);
+		} else {
+			temp.song = song;
+			temp.end = true;
+		}
+	}
+
+	public TrieNode bin_search(char c, ArrayList<TrieNode> t, int p, int q) {
+		if (p > q) {
+			return null;
+		}
+		int n = (q + p + 1) / 2;
+		char curr = t.get(n).letter;
+		if (p == q) {
+			if (t.get(q).letter == c) {
+				return t.get(q);
+			} else {
+				return null;
+			}
+		}
+		if (q - p <= 2) {
+			for (int i = p; i < q + 1; i++) {
+				if (t.get(i).letter == c)
+					return t.get(i);
+			}
+			return null;
+		} else if (curr == c) {
+			return t.get(n);
+		} else if (curr < c) {
+			return bin_search(c, t, n + 1, q);
+
+		} else {
+			return bin_search(c, t, 0, n - 1);
+		}
+	}
+
+	protected TrieNode exists(String word) {
+		if (word.length() == 0)
+			return null;
+		char current = word.charAt(0);
+		TrieNode temp = bin_search(current, this.children, 0, this.children
+				.size() - 1);
+		if (temp != null && word.length() > 1) {
+			return temp.exists(word.substring(1));
+		} else if (temp != null && temp.end == true && word.length() == 1) {
+			return temp;
+		} else if (temp != null && word.length() == 1) {
+			return temp;
+		} else
+			return null;
+
+	}
+	/*
+	public boolean delete(String word) {
+		char current = word.charAt(0);
+		TrieNode temp = bin_search(current, this.children, 0, 1);
+		if (temp != null && word.length() > 1) {
+			if (temp.delete(word.substring(1)) == true) {
+				if (temp.children.size() == 0)
+					return true;
+			} else
+				return false;
+		} else if (temp != null && temp.end == true && word.length() == 1) {
+			temp.end = false;
+			temp.song = null;
+			return true;
+		} else
+			return false;
+	}
+
+	*/
+	
+	private boolean isEmpty(TrieNode trieNode) {
+		if (trieNode.children.size() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public ArrayList<Song> retSongs(String word) {
+		ArrayList<Song> songNames = new ArrayList();
+		TrieNode temp = exists(word);
+		if (temp == null)
+			return null;
+		if (temp.end == true) {
+			songNames.add(temp.song);
+		}
+		auto_complete_Songs(temp, songNames);
+		return songNames;
+
+	}
+
+	public void auto_complete_Songs(TrieNode temp, ArrayList<Song> songNames) {
+		if (temp.children == null) {
+			return;
+		}
+		for (int i = 0; i < temp.children.size(); i++) {
+			if (temp.children.get(i).end == true) {
+				songNames.add(temp.children.get(i).song);
+			}
+			auto_complete_Songs(temp.children.get(i), songNames);
+		}
+		return;
+	}
+
+	public void addAll(Song[] songs) {
+		for (int i = 0; i < songs.length; i++) {
+			String s = song.getTitle();
+			add(s, song);
+		}
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		if (this.letter < ((TrieNode) arg0).letter)
+			return 0;
+		else
+			return 1;
+	}
+
+}
